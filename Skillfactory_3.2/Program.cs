903126a8a87757c41e93dc91b3cab69ffa76298e
Serj112 <raskolnikov.112@mail.ru>
2022-10-22 00:09:23 +0300

    Add project files.

---
+++ Skillfactory_3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string MyName = "Serj";
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\n Привет, мир!");
+        Console.WriteLine("\n Мне 28 лет");
+        Console.WriteLine("\n Моё имя \t {0}", MyName);
+        Console.WriteLine("\u0023");
+        Console.ReadLine();
+    }
+}
