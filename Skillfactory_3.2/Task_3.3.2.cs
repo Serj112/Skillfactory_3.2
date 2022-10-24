b984fcc88bfd5aa1151ab0bb6b8cc2ad3297821e
Serj112 <raskolnikov.112@mail.ru>
2022-10-24 12:29:48 +0300

    Task 3.3.2

---
+++ Skillfactory_3.2/Task_3.3.2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    
+        string MyName = "Serj";
+        byte Age = 28;
+        bool Pet = true;
+        byte Size = 44;
+        Console.WriteLine("What is your name?");
+        Console.WriteLine("My name is {0}", MyName);
+        Console.WriteLine("How old are you?");
+        Console.WriteLine("I am {0}", Age);
+        Console.WriteLine("Do you have a pet?");
+        Console.WriteLine(Pet);
+        Console.WriteLine("What is your shoes size?");
+        Console.WriteLine("My shoes size is {0}", Size);
+        Console.ReadLine();
