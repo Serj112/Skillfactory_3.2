bcb0c1ead9404a5ac28fe8bc198d745d72d1a6a6
Serj112 <raskolnikov.112@mail.ru>
2022-10-26 18:09:52 +0300

    Tasks 3.4 - 3.7

---
+++ Skillfactory 3.7/3.7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your age:");
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} \n What is your favorite day of week?", name, age);
+        DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0} \nEnter your birthdate:", day);
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is {0}", birthdate);
+    }
+
+}
