266cb1881c91fcd0781c6b1e8857236241d16210
Serj112 <raskolnikov.112@mail.ru>
2022-10-31 16:28:30 +0300

    Task 4.1

---
+++ Skillfactory 4/4.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var Color = Console.ReadLine();
+        if (Color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Your color is red!");
+        }
+        else if (Color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Your color is cyan!");
+        }
+    }
+}
